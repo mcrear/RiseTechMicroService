4a0ccc0739432000d16c2031f50bd71d76b93c0d
mcrear <mcrear.master@gmail.com>
2021-11-16 19:40:37 +0300

    Ocelot.json oluşturulması ve projelerin haberleştiğinin test edilmesi

---
+++ RiseTech.Report/Controllers/ReportController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RiseTech.Report.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Rprt 1", "Rprt 2" };
+        }
+    }
+}
