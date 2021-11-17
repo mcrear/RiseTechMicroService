e249ea8144efef77b73032ef93d8f80c9c1f6f85
mcrear <mcrear.master@gmail.com>
2021-11-17 08:59:50 +0300

    Http extension ve IContactService interface ve classlarının hazırlanması

---
+++ RiseTech.BasicWeb/Services/ContactService.cs
@@ -0,0 +1,8 @@
+﻿using RiseTech.BasicWeb.Services.Interfaces;
+
+namespace RiseTech.BasicWeb.Services
+{
+    public class ContactService : IContactService
+    {
+    }
+}
---
+++ RiseTech.BasicWeb/Services/Interfaces/IContactService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RiseTech.BasicWeb.Services.Interfaces
+{
+    public interface IContactService
+    {
+    }
+}
