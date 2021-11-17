4b861330daea2f5c2a9b5352a2fba583d282ccf2
mcrear <mcrear.master@gmail.com>
2021-11-17 09:03:40 +0300

    IContactService içeriğinin belirlenmesi

---
+++ RiseTech.BasicWeb/Models/PersonInformationModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RiseTech.BasicWeb.Models
+{
+    public class PersonInformationModel
+    {
+    }
+}
--- RiseTech.BasicWeb/Services/Interfaces/IContactService.cs
+++ RiseTech.BasicWeb/Services/Interfaces/IContactService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RiseTech.BasicWeb.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,11 @@ namespace RiseTech.BasicWeb.Services.Interfaces
 {
     public interface IContactService
     {
+        Task<IEnumerable<PersonModel>> GetCatalog();
+        Task<IEnumerable<PersonModel>> GetPersonByCompany(string company);
+        Task<IEnumerable<PersonModel>> GetPersonByFirstName(string firstName);
+        Task<IEnumerable<PersonModel>> GetPersonByLastName(string lastName);
+        Task<PersonModel> GetPerson(string id);
+        Task<PersonModel> CreatePerson(PersonModel model);
     }
 }
